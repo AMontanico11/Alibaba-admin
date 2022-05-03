9ded35d1ef905785adff44fafabb76499f631bdb
Montanico <afpirir@scl.edu.gt>
2022-05-02 22:21:29 -1200

    Se agregan los datos de la empresa en la base de datos

---
+++ js/category.js
@@ -0,0 +1,10 @@
+let order =[
+    /**CATEGORIA DE PRODUCTOS CON SU CANTIDAD DADA */
+    [4000,	'Laptops']
+    [4100,	'Mobile phones']
+    [4200,	'Tablets']
+    [4300,	'Servers']
+    [4400,	'PC']
+    [4500,	'Cables']
+    [4600,	'Headphones']
+]
---
+++ js/order_placed_user.js
@@ -0,0 +1,10 @@
+let order = [
+    /*DATOS DEL NUMERO DEL USUARIO JUNTO AL PUESTO QUE OCUPA*/
+    ['100',	1000]
+    ['101',	1001]
+    ['102',	1002]
+    ['103',	1003]
+    ['104',	1004]
+    ['105',	1005]
+    ['106',	1006]
+]
